4c085e48f4a4e7af08538bee02217c4d523b678d
Emman <manugasemmanuell@gmail.com>
2023-10-07 20:35:56 +0800

    inster new line 11 dev 2

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ console.log("rebase 1");
 
 console.log("rebase branch 1");
 console.log("new line merge");
+console.log("dev2 branch2 1 line11");
 console.log("rebase branch 2");
 console.log("rebase branch new branch7");
