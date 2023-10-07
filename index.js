f67ad0eed47e8f0330aff4ceaddc95fc7f99fde4
Emman <manugasemmanuell@gmail.com>
2023-10-07 20:39:08 +0800

    new commit branch8

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log("branch 2");
 
 console.log("rebase 1");
 
-console.log("rebase branch 1");
+console.log("rebase branch new");
 console.log("new line merge");
 console.log("rebase branch 2");
 console.log("rebase branch new branch7");
